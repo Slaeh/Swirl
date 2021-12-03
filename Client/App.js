bb9949a96086e1dbc20a07859faf3a20a5e09970
Harjit <harjitliyal@gmail.com>
2021-12-03 13:23:52 -0500

    Add NativeBase

--- Client/App.js
+++ Client/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
