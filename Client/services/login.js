94fa686e073410d483cf558377ef3617f1bf2923
Edward Paez <Paez.edward5@gmail.com>
2021-12-08 13:07:10 -0500

    Finish front page Login auth and user sign up

--- Client/services/login.js
+++ Client/services/login.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const baseUrl = "http://localhost:5000/api/login";
 
 const login = async (credentials) => {
+  console.log("YESSIR");
   const response = await axios.post(baseUrl, credentials);
   return response.data;
 };
